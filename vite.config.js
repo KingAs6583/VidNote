d09af371b90d1ab966a24dae87b294303c6a3e20
KingAs6583 <starabbu007@gmail.com>
2024-12-24 00:43:00 +0530

    add publish config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+/** publish
+ * npm install gh-pages --save-dev
+ * npm run build 
+ * npm run deploy
+ */
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/VidNote/"
 })
